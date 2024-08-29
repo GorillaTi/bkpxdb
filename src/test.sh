ed10c5ebbd46cd99c5cc9ee02f6e5d47f40824d4
EK-IT <ed.cespedesa@gmail.com>
2024-08-29 09:16:47 -0400

    optimizacion de logs

--- src/test.sh
+++ src/test.sh
@@ -7,4 +7,4 @@
 # Version: 2.0.0
 # FUNSION:
 # Captura de Usuario y fecha de ejecucion del script
-echo "$(whoami) [$(date +%Y-%m-%d) - $(date +%H:%M:%S)]" >> /var/log/cron-test.log 2>&1
+echo "$(whoami) [$(date +%Y-%m-%d) - $(date +%H:%M:%S)]" >> /var/log/cron/cron-test.log 2>&1
