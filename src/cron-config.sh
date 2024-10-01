c0edb2c171928a5f6033cc154fb2a77318ac83e1
EK-IT <ed.cespedesa@gmail.com>
2024-10-01 13:31:52 -0400

    optimizacion y reconfiguracion

--- src/cron-config.sh
+++ src/cron-config.sh
@@ -1,35 +1,14 @@
 #!/usr/bin/env bash
 
-# Autor:  Edmundo Cespedes A.
+# Copyright (c) 2024 Edmundo Cespedes A. <a.k.a. eksys>
+# Author:  Edmundo Cespedes A.
 # Licencia: MIT
-# NOMBRE: run.sh
-# Version: 1.0.0
-# FUNSION:
+# https://github.com/GorillaTi/bkpxdb/raw/main/LICENSE
+# Name Script: run.sh
+# Function:
 # Configura  la hora de ejecucion del script  de copia de seguridad de las de bases de
 # datos MYSQL MariaDn y PosrgreSQL
-
-# Copyright (c) 2024 Edmundo Cespedes A. <a.k.a. eksys>
-
-# This software is provided under the MIT License.
-
-# You can obtain a copy of the license at https://opensource.org/licenses/MIT
-
-# In summary, you are permitted to use, copy, modify, merge, publish, distribute, 
-# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
-#  is furnished to do so, subject to the following conditions:
-
-# You must include this copyright notice and this license in all copies or substantial 
-# portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
-# OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
+# Version: 2.0.0
 
 # Exit if any command fails
 set -eou pipefail
@@ -37,11 +16,11 @@ set -eou pipefail
 LOGS="/var/log/cron/cron.log"
 
 {
-    echo "Actualizando crontab";
-    cp /app/crontab/crontab /etc/cron.d/bkpxdb-cron;
-    chown root:root /etc/cron.d/bkpxdb-cron;
-    chmod 644 /etc/cron.d/bkpxdb-cron ;
+    echo "Actualizando crontab"
+    cp /app/crontab/crontab /etc/cron.d/bkpxdb-cron
+    chown root:root /etc/cron.d/bkpxdb-cron
+    chmod 644 /etc/cron.d/bkpxdb-cron
 
     echo "Starting crond... $(date)"
-    service cron  restart 
-} >> "$LOGS" 2>&1
+    service cron restart
+} >>"$LOGS" 2>&1
